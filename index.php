f957602373953be196b8072595b0085dc29633ba
Mooyi07 <120180277+Mooyi07@users.noreply.github.com>
2024-03-02 01:24:17 +0800

    Commit Folders, html, css

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
-<?php
-    echo "Fund-E Login Page";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fund-E | Login</title>
+    <link rel="stylesheet" href="styles.scss">
+    <link rel="icon" href="assets/img/fundi logo.svg" type="image/x-icon">
+</head>
+<body>
+<main>
+    <div class="content">
+        <div class="logo-and-name">
+            <img id="logo" src="assets/img/fundi logo.svg">
+            <h1>Fund-e</h1>
+        </div>
+        <div class="slogan">
+            <h1>
+                Track your Expenses <br> 
+                Financial Discipline <br> 
+                Well-defined options <br>
+                Know your goals with <br>
+                <span>Fund-e</span>
+            </h1>
+        </div>
+    </div>
+    <div class="login-form">
+        <div>
+            <h1>
+                Track your Expenses <br> 
+                Financial Discipline <br> 
+                Well-defined options <br>
+                Know your goals with <br>
+                <span>Fund-e</span>
+            </h1>
+        </div>
+    </div>
+</main>
+</body>
+</html>

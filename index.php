ae21cf67655ba0ac08b4b3e1e518bb703d3f49ff
Mooyi07 <120180277+Mooyi07@users.noreply.github.com>
2024-03-02 23:26:17 +0800

    Finalized Design

--- index.php
+++ index.php
@@ -7,29 +7,40 @@
     <title>Fund-E | Login</title>
     <link rel="stylesheet" href="styles.scss">
     <link rel="icon" href="assets/img/logo.svg" type="image/x-icon">
-    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
 </head>
 <body>
 <main>
     <div class="content">
-        <div class="logo-and-name">
-            <img id="logo" src="assets/img/logo.svg">
-            <h1>Fund-e</h1>
-        </div>
-        <div class="slogan">
-            <h1>
-                Track your Expenses <br> 
-                Financial Discipline <br> 
-                Well-defined options <br>
-                Know your goals with <br>
-                <span>Fund-e</span>
-            </h1>
+        <div class="opac"> 
+            <div class="logo-and-name">
+                <img id="logo" src="assets/img/logo.svg">
+                <h1>Fund-e</h1>
+            </div>
+            <div class="slogan">
+                <h1>
+                    Track your Expenses <br> 
+                    Financial Discipline <br> 
+                    Well-defined options <br>
+                    Know your goals with <br>
+                    <span>Fund-e</span>
+                </h1>
+            </div>
         </div>
     </div>
     <div class="login-form">
         <form action="action_page.php" method="post">
             <h1>LOGIN</h1>
 
+            <div class="upper-container">
+                <input id="uname" type="text" placeholder="Username" name="uname" required><br>
+                <input id="psw" type="password" placeholder="Password" name="psw" required><br>
+                <button type="submit">LOGIN</button>
+            </div>
+
+            <div class="lower-container">
+                <a href="#">Sign Up</a>
+                <a href="#">Forgot Password</a></span>
+            </div>
         </form>
     </div>
 </main>

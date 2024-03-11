f6ec6d51c35b6752e9a2212bb9d15ad56f2d2f9c
Mooyi07 <120180277+Mooyi07@users.noreply.github.com>
2024-03-11 21:13:24 +0800

    Finished Main Page Stream

---
+++ main/index.php
@@ -0,0 +1,31 @@
+<?php 
+    session_abort();
+    session_unset();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fund-E | Login</title>
+    <link rel="stylesheet" href="styles.scss">
+    <link rel="icon" href="../assets/img/logo.svg" type="image/x-icon">
+</head>
+<body>
+<main>
+    <div class="content">
+        <div class="opac"> 
+            <div class="logo-and-name">
+                <img id="logo" src="../assets/img/logo.svg">
+                <h1>Fund-e</h1>
+            </div>
+        </div>
+        <div class="welcome">
+            <h1>Welcome to <br><span>Fund-e</span>!</h1>
+            <a ref="home/">PROCEED</a>
+        </div>
+    </div>
+</main>
+</body>
+</html>
